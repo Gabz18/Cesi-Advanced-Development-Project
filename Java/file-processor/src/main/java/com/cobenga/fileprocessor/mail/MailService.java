c42632594c4b15452ed81951cd2e0351bea7cc14
gabz18 <gabinritz@gmail.com>
2020-06-26 15:08:48 +0200

    Implemented Mail Service

--- Java/file-processor/src/main/java/com/cobenga/fileprocessor/mail/MailService.java
+++ Java/file-processor/src/main/java/com/cobenga/fileprocessor/mail/MailService.java
@@ -1,5 +1,11 @@
 package com.cobenga.fileprocessor.mail;
 
+import org.apache.commons.mail.EmailException;
+
+import javax.ejb.Local;
+
+@Local
 public interface MailService {
 
+    void sendMail(String to, String subject, String message) throws EmailException;
 }
--- Java/file-processor/src/main/java/com/cobenga/fileprocessor/mail/MailServiceBean.java
+++ Java/file-processor/src/main/java/com/cobenga/fileprocessor/mail/MailServiceBean.java
@@ -1,4 +1,27 @@
 package com.cobenga.fileprocessor.mail;
 
-public class MailServiceBean {
+import org.apache.commons.mail.Email;
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.SimpleEmail;
+
+import javax.ejb.Stateless;
+
+@Stateless
+public class MailServiceBean implements MailService {
+
+    private static final String SMTP_HOST = "localhost";
+    private static final int SMTP_PORT = 8082;
+    private static final String MAIl_FROM = "Encrypted File Processor";
+
+    @Override
+    public void sendMail(String to, String subject, String message) throws EmailException {
+        Email email = new SimpleEmail();
+        email.setHostName(SMTP_HOST);
+        email.setSmtpPort(SMTP_PORT);
+        email.setFrom(MAIl_FROM);
+        email.addTo(to);
+        email.setSubject(subject);
+        email.setMsg(message);
+        email.send();
+    }
 }
