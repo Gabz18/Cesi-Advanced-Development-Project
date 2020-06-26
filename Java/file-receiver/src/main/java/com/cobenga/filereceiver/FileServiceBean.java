cae20a1e55483ce8b0fa03cdd679c637afccdf5b
gabz18 <gabinritz@gmail.com>
2020-06-26 18:25:20 +0200

    Added documentation to file-receiver

--- Java/file-receiver/src/main/java/com/cobenga/filereceiver/FileServiceBean.java
+++ Java/file-receiver/src/main/java/com/cobenga/filereceiver/FileServiceBean.java
@@ -9,6 +9,9 @@ import javax.jms.Queue;
 import javax.jms.TextMessage;
 import javax.jws.WebService;
 
+/**
+ * @author gabz18
+ */
 @Stateless
 @WebService(
         endpointInterface = "com.cobenga.filereceiver.FileServiceEndpointInterface",
@@ -23,12 +26,16 @@ public class FileServiceBean implements FileServiceEndpointInterface {
     @Resource(lookup = "jms/decryptedFileQueue")
     private Queue decryptedFileQueue;
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    public void handleIncomingFile(String fileName, String code, String decryptedText, String userEmail) throws JMSException {
+    public void handleIncomingFile(String fileName, String fileUuid, String code, String decryptedText, String userEmail) throws JMSException {
         TextMessage textMessage = jmsContext.createTextMessage(decryptedText);
         textMessage.setStringProperty("fileName", fileName);
         textMessage.setStringProperty("code", code);
         textMessage.setStringProperty("userEmail", userEmail);
+        textMessage.setStringProperty("fileUuid", fileUuid);
         jmsContext.createProducer().send(decryptedFileQueue, textMessage);
     }
 }
--- Java/file-receiver/src/main/java/com/cobenga/filereceiver/FileServiceEndpointInterface.java
+++ Java/file-receiver/src/main/java/com/cobenga/filereceiver/FileServiceEndpointInterface.java
@@ -5,12 +5,26 @@ import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebService;
 
+/**
+ * @author gabz18
+ */
 @WebService(name = "FileEndpoint")
 public interface FileServiceEndpointInterface {
 
+    /**
+     * Forwards the incoming file to be processed, to the JMS queue.
+     *
+     * @param fileName The file name.
+     * @param fileUuid The file internal Uuid to the Information System.
+     * @param code The decryption code used to obtain this file content.
+     * @param decryptedText The decrypted file content.
+     * @param userEmail The current asking for this file process.
+     * @throws JMSException
+     */
     @WebMethod(operationName = "fileAnalysisProcessStart")
     void handleIncomingFile(
             @WebParam(name = "fileName") String fileName,
+            @WebParam(name = "File internal UUID") String fileUuid,
             @WebParam(name = "code") String code,
             @WebParam(name = "decryptedText") String decryptedText,
             @WebParam(name = "userEmail") String userEmail
