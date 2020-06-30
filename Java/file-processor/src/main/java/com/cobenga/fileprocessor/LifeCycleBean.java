f7da608d1c7814fc78a78100fd082864934f722f
gabz18 <gabinritz@gmail.com>
2020-06-30 13:42:02 +0200

    Configured Database connection

--- Java/file-processor/src/main/java/com/cobenga/fileprocessor/LifeCycleBean.java
+++ Java/file-processor/src/main/java/com/cobenga/fileprocessor/LifeCycleBean.java
@@ -1,15 +1,37 @@
 package com.cobenga.fileprocessor;
 
+import com.cobenga.fileprocessor.dao.WordDao;
+import com.cobenga.fileprocessor.dao.WordDaoImpl;
+
 import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
+import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @Singleton
 @Startup
 public class LifeCycleBean {
 
+    private static final String PATH_TO_DICTIONARY =
+            "C:/Users/gabin/Desktop/Cesi-Advanced-Development-Project/Java/file-processor/src/main/resources/dictionary.txt";
+
+    @EJB
+    private WordDao wordDao;
+
     @PostConstruct
     public void init() {
-        // TODO fill database
+        wordDao.reInitDb();
+        File file = new File(PATH_TO_DICTIONARY);
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                wordDao.insertWord(line);
+            }
+        } catch (Exception e) {
+            Logger.getLogger(LifeCycleBean.class.getName()).log(Level.SEVERE, null, e);
+        }
     }
 }
--- Java/file-processor/src/main/java/com/cobenga/fileprocessor/dao/WordDao.java
+++ Java/file-processor/src/main/java/com/cobenga/fileprocessor/dao/WordDao.java
@@ -15,4 +15,18 @@ public interface WordDao {
      * @return A boolean representing whether or not the word has been found.
      */
     boolean wordExists(String word);
+
+    /**
+     * Inserts a new word into the database dictionary.
+     *
+     * @param word The word to insert.
+     * @return True if the word has been inserted.
+     */
+    boolean insertWord(String word);
+
+    /**
+     * Deletes and recreates the database.
+     *
+     */
+    void reInitDb();
 }
